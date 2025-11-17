abb82f6d673fdf8369751424dd88347a261b836f
Caner Giden <95746736+caner0706@users.noreply.github.com>
2025-11-18 00:34:47 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,20 +1,22 @@
+# app/main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.config import settings
 from app.db import init_db
-from app.routers import health, workflows
+from app.routers import health, workflows, auth   # 👈 auth da import edildi
 
 
 def create_app() -> FastAPI:
     app = FastAPI(
         title=settings.APP_NAME,
         version=settings.APP_VERSION,
-        docs_url="/",                 # 👈 Swagger root’ta
-        redoc_url=None,               # İstersen açabilirsin
-        openapi_url="/openapi.json",  # OpenAPI JSON
+        docs_url="/",                 # Swagger root’ta
+        redoc_url=None,
+        openapi_url="/openapi.json",
     )
 
+    # CORS
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
@@ -25,10 +27,9 @@ def create_app() -> FastAPI:
 
     # Routers
     app.include_router(health.router)
-    app.include_router(auth.router, prefix="/api")       # 👈 /api/auth/...
-    app.include_router(workflows.router, prefix="/api")  # /api/workflows/..
+    app.include_router(auth.router, prefix="/api")       # /api/auth/...
+    app.include_router(workflows.router, prefix="/api")  # /api/workflows/...
 
-    
     @app.on_event("startup")
     def on_startup():
         init_db()
